cdf3487cf235402d91c0c962ac135f4248ebf8ea
Louis <louis.charpentier@ymail.com>
2015-11-20 11:00:04 +0100

    pom.xml

---
+++ .idea/src/java/td/App.java
@@ -0,0 +1,16 @@
+package td;
+import javax.swing.JFrame;
+
+/**
+ * Created by Louis on 20/11/2015.
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Arthur a le seum");
+//Possibilité 1 : instanciation avec le libellé
+        Controller bouton = new Controller();
+
+    }
+
+}
---
+++ .idea/src/java/td/Controller.java
@@ -0,0 +1,33 @@
+package td;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ * Created by Louis on 20/11/2015.
+ */
+public class Controller extends JFrame{
+    private JPanel pan = new JPanel();
+    private JButton boutonEURDepart = new JButton("EUR");
+    private JButton boutonUSDepart = new JButton("US");
+    private JButton boutonGPDDepart = new JButton("GBP");
+
+
+    public Controller(){
+        this.setTitle("Convetisseur");
+        this.setSize(300, 150);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        //Ajout du bouton à notre content pane
+        pan.add(boutonEURDepart);
+        this.setContentPane(pan);
+        this.setVisible(true);
+        pan.add(boutonGPDDepart);
+        this.setContentPane(pan);
+        this.setVisible(true);
+        pan.add(boutonUSDepart);
+        this.setContentPane(pan);
+        this.setVisible(true);
+
+    }
+}
