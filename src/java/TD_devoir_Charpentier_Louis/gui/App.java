4fa36587f805e25ea0eb1d8f81ab063d930b3a89
Louis <louis.charpentier@ymail.com>
2015-11-21 17:54:59 +0100

    pom.xml

--- src/java/TD_devoir_Charpentier_Louis/gui/App.java
+++ src/java/TD_devoir_Charpentier_Louis/gui/App.java
@@ -1,20 +1,230 @@
 package TD_devoir_Charpentier_Louis.gui;
 import TD_devoir_Charpentier_Louis.model.Controller;
-
-import javax.swing.JPanel;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
 
 /**
  * Created by Louis on 20/11/2015.
  */
-public class App {
-
+public class App extends Application {
+    private String fromMoney;
+    private String toMoney;
     public static void main(String[] args) {
 
+        Application.launch(App.class, args);
+    }
+    public String caclul(String fromMoney,String toMoney,String value)
+    {
+        return "";
+    }
+    public void setValue(int value,String fromOrTo)
+    {
+        if(fromOrTo=="from")
+        {
+            if(value==0)
+            {}
+        }
+    }
+    @Override
+    public void start(Stage primaryStage) {
 
-         //Possibilité 1 : instanciation avec le libellé
-           Controller bouton = new Controller();
 
+        primaryStage.setTitle("Convertisseur!");
+        Group root= new Group();
+        Scene scene = new Scene(root,300,250,Color.GRAY);
+        TextField numberFrom=new TextField();
+        numberFrom.setPrefSize(75, 12);
+        numberFrom.setLayoutX(20);
+        numberFrom.setLayoutY(20);
+
+        TextField numberTo=new TextField();
+        //numberTo.setDisable(true);
+        numberTo.setPrefSize(75,12);
+        numberTo.setLayoutX(210);
+        numberTo.setLayoutY(20);
+
+
+        Button EURFrom = new Button();
+        EURFrom.setText("EUR");
+        EURFrom.setLayoutX(20);
+        EURFrom.setLayoutY(100);
+        EURFrom.setOnAction(new EventHandler<ActionEvent>() {
+
+            public void handle(ActionEvent event) {
+                if(EURFrom.getTextFill()==Color.RED)
+                {
+                    EURFrom.setTextFill(Color.BLACK);
+
+                }
+                else
+                {EURFrom.setTextFill(Color.RED);
+                    fromMoney="EUR";
+
+                }
+
+            }
+        });
+
+        Button USDFrom = new Button();
+        USDFrom.setText("USD");
+        USDFrom.setLayoutX(20);
+        USDFrom.setLayoutY(130);
+        USDFrom.setOnAction(new EventHandler<ActionEvent>() {
+
+            public void handle(ActionEvent event)
+            {
+                if(USDFrom.getTextFill()==Color.RED)
+            {
+                USDFrom.setTextFill(Color.BLACK);
+
+            }
+            else
+            {
+                USDFrom.setTextFill(Color.RED);
+                EURFrom.setTextFill(Color.BLACK);
+                fromMoney="USD";
+
+            }
+            }
+        });
+
+        Button GBDFrom = new Button();
+        GBDFrom.setText("GBD");
+        GBDFrom.setLayoutX(20);
+        GBDFrom.setLayoutY(160);
+        GBDFrom.setOnAction(new EventHandler<ActionEvent>() {
+
+            public void handle(ActionEvent event) {
+
+                {
+                    if(GBDFrom.getTextFill()==Color.RED)
+                    {
+                        GBDFrom.setTextFill(Color.BLACK);
+
+                    }
+                    else
+                    {
+                        GBDFrom.setTextFill(Color.RED);
+                        EURFrom.setTextFill(Color.BLACK);
+                        USDFrom.setTextFill(Color.BLACK);
+                        fromMoney="GBD";
+
+                    }
+                }
+            }
+        });
+
+        Button GBDTo = new Button();
+        GBDTo.setText("GBD");
+        GBDTo.setLayoutX(250);
+        GBDTo.setLayoutY(160);
+        GBDTo.setOnAction(new EventHandler<ActionEvent>() {
+
+            public void handle(ActionEvent event) {
+                {
+                    if(GBDTo.getTextFill()==Color.RED)
+                    {
+                        GBDTo.setTextFill(Color.BLACK);
+
+                    }
+                    else
+                    {
+                        GBDTo.setTextFill(Color.RED);
+                        toMoney="GBD";
+
+                    }
+                }
+            }
+        });
+
+        Button USDTo = new Button();
+        USDTo.setText("USD");
+        USDTo.setLayoutX(250);
+        USDTo.setLayoutY(130);
+        USDTo.setOnAction(new EventHandler<ActionEvent>() {
+
+            public void handle(ActionEvent event) {
+                {
+                    if(USDTo.getTextFill()==Color.RED)
+                    {
+                        USDTo.setTextFill(Color.BLACK);
+
+                    }
+                    else
+                    {
+                        USDTo.setTextFill(Color.RED);
+                        GBDTo.setTextFill(Color.BLACK);
+                        toMoney="USD";
+
+                    }
+                }
+            }
+        });
+
+        Button EURTo = new Button();
+        EURTo.setText("EUR");
+        EURTo.setLayoutX(250);
+        EURTo.setLayoutY(100);
+        EURTo.setOnAction(new EventHandler<ActionEvent>() {
+
+            public void handle(ActionEvent event) {
+                {
+                    if(EURTo.getTextFill()==Color.RED)
+                    {
+                        EURTo.setTextFill(Color.BLACK);
+
+                    }
+                    else
+                    {
+                        EURTo.setTextFill(Color.RED);
+                        GBDTo.setTextFill(Color.BLACK);
+                        USDTo.setTextFill(Color.BLACK);
+                        toMoney="EUR";
+
+                    }
+                }
+            }
+        });
+
+        Button calcul = new Button();
+        calcul.setText("Calcul");
+        calcul.setLayoutX(125);
+        calcul.setLayoutY(225);
+        calcul.setOnAction(new EventHandler<ActionEvent>() {
+
+            public void handle(ActionEvent event) {
+                numberTo.setText(calcul(fromMoney,toMoney,numberFrom.getText()));
+            }
+        });
+
+        root.getChildren().add(EURFrom);
+        root.getChildren().add(GBDFrom);
+        root.getChildren().add(USDFrom);
+        root.getChildren().add(EURTo);
+        root.getChildren().add(USDTo);
+        root.getChildren().add(GBDTo);
+        root.getChildren().add(numberFrom);
+        root.getChildren().add(numberTo);
+        root.getChildren().add(calcul);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
+    private String calcul(String fromMoney,String toMoney,String value)
+    {
+        if(fromMoney=="EUR")
+        {
+            if(toMoney=)
+        }
+        return "";
+    }
 }
--- src/java/TD_devoir_Charpentier_Louis/model/Controller.java
+++ src/java/TD_devoir_Charpentier_Louis/model/Controller.java
@@ -1,24 +1,31 @@
 package TD_devoir_Charpentier_Louis.model;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
 /**
  * Created by Louis on 20/11/2015.
  */
-public class Controller extends JFrame{
-    private JPanel pan = new JPanel();
+public class Controller {
+   /* private JPanel pan = new JPanel();
     private JButton boutonEURDepart = new JButton("EUR");
     private JButton boutonUSDepart = new JButton("US");
-    private JButton boutonGPDDepart = new JButton("GBP");
+    private JButton boutonGPDDepart = new JButton("GBP");*/
 
 
-    public Controller(){
-        this.setTitle("Convetisseur");
+    public Controller() {
+
+      /*  this.setTitle("Convetisseur");
         this.setSize(300, 150);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
-        //Ajout du bouton à notre content pane
+
+
+
         pan.add(boutonEURDepart);
         this.setContentPane(pan);
         this.setVisible(true);
@@ -27,7 +34,8 @@ public class Controller extends JFrame{
         this.setVisible(true);
         pan.add(boutonUSDepart);
         this.setContentPane(pan);
-        this.setVisible(true);
+        this.setVisible(true)*/
+
 
     }
 }
